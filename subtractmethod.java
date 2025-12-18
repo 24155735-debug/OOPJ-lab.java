1eb0f9aa712dc975c728e7c6e75e9b3ae33011c9
24155735-debug <24155735@kiit.ac.in>
2025-12-19 02:06:37 +0530

    Subtract method code upload

---
+++ subtractmethod.java
@@ -0,0 +1,18 @@
+class subtractmethod {
+    public static void main(String[] args) {
+        int a = 14;
+        int b = 4;
+        int c = 2;
+        int s1 = a - b; // subtract two integer values
+        int s2 = a - b - c; // subtract three integer values
+        double x = 96.69;
+        double y = 54.45;
+        double z = 21.12;
+        double s3 = x - y; // subtract two double values
+        double s4 = x - y - z; // subtract three double values
+        System.out.println("Subtract two integer values:" + s1);
+        System.out.println("Subtract three integer values:" + s2);
+        System.out.println("Subtract two double values:" + s3);
+        System.out.println("Subtract three double values: " + s4);
+    }
+}
